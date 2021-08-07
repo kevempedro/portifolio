50b41c1c6447ff732fa6baf888e73e6a1f96c728
kevempedro <kevempereira520@gmail.com>
2021-08-07 10:52:41 -0300

    Portifolio V1

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{});
+sr.reveal('.button',{delay: 200});
+sr.reveal('.home__img',{delay: 400});
+sr.reveal('.home__social-icon',{ interval: 200});
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{});
+sr.reveal('.about__subtitle',{delay: 400});
+sr.reveal('.about__text',{delay: 400});
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{});
+sr.reveal('.skills__text',{});
+sr.reveal('.skills__data',{interval: 200});
+sr.reveal('.skills__img',{delay: 600});
+
+/*SCROLL PROJECTS*/
+sr.reveal('.project-title',{interval: 200});
+sr.reveal('.project-text',{interval: 200});
+sr.reveal('.project-content-images',{delay: 600});
+sr.reveal('.project-content-title',{delay: 600});
+sr.reveal('.project-content-text',{interval: 600});
+
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact-container',{interval: 200});
